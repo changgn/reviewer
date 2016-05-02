bba53fdd1c50ead2cfa0e9d46b6218c6489bae05
changgeun <kjs_lovey@naver.com>
2016-05-02 21:16:36 +0900

    recommend decorators.xml 추가

--- src/action/recommend/RecommendProAction.java
+++ src/action/recommend/RecommendProAction.java
@@ -9,9 +9,6 @@ public class RecommendProAction implements CommandAction {
 	 public String requestPro(HttpServletRequest request, HttpServletResponse response)throws Throwable{
 		 
 		 
-		 
-		 
-		 
 		 return "/recommend/recommendPro.jsp";
 	 }
 	
